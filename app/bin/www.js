2298c3c24d0ab0b346439cb9b3f272198be7961f
congs00n <ninxtxn@gmail.com>
2022-07-17 02:36:12 +0900

    ejs - css연결

--- app/bin/www.js
+++ app/bin/www.js
@@ -5,7 +5,7 @@ const app = require("../app.js");
 
 // 서버 실행시키는 코드가 여기 있으니 이걸 실행시켜야 한다. 
 app.listen(PORT, () => {
-  console.log("서버 가동");
+  console.log("서버 가동 ==> PORT : "+PORT);
 });
 // 이러면 앱이라는 것을 찾을 수가 없으니! 
 // 불러와야 함!!
